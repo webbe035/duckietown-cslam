fae50bcb9f777e26da5cb7e4e53f3441b1b0ac71
AmaurX <amaury.camus@gmail.com>
2019-04-07 18:06:02 +0200

    added generation of watchtower

--- unit-test-generator/unit-test-generator.py
+++ unit-test-generator/unit-test-generator.py
@@ -24,7 +24,7 @@ offset_position = g2o.Isometry3d(R_offset, t_offset)
 
 
 class Watchtower(object):
-    def __init__(self, name, measure_noise, position = None):
+    def __init__(self, name, measure_noise, position=None):
         self.measure_noise = measure_noise
         self.name = name
         if(position != None):
@@ -32,11 +32,11 @@ class Watchtower(object):
         else:
             print("no position given to %s" % self.name)
             self.position = None
-    
+
     def sees_apriltag(self, apriltag):
         if(apriltag.isinstance(Apriltag)):
             real_transform = self.position.inverse() * apriltag.position
-            #apply noise 
+            # apply noise
             return real_transform
         else:
             print("The object is not apriltag")
@@ -44,13 +44,14 @@ class Watchtower(object):
     def sees_duckiebot(self, duckiebot):
         if(duckiebot.isinstance(Duckiebot)):
             real_transform = self.position.inverse() * duckiebot.position
-            #apply noise 
+            # apply noise
             return real_transform
         else:
             print("The object is not apriltag")
 
+
 class Duckiebot(object):
-    def __init__(self, name, measure_noise, initial_position = None)
+    def __init__(self, name, measure_noise, initial_position=None):
         self.name = name
         self.measure_noise = measure_noise
         if(initial_position != None):
@@ -62,58 +63,87 @@ class Duckiebot(object):
     def sees_apriltag(self, apriltag):
         if(apriltag.isinstance(Apriltag)):
             real_transform = self.position.inverse() * apriltag.position
-            #apply noise 
+            # apply noise
             return real_transform
         else:
             print("The object is not apriltag")
 
+
 class Apriltag(object):
-    def __init__(self, name, position = None):
+    def __init__(self, name, position=None):
         self.name = name
         if(position != None):
             self.position = offset_position * position
         else:
             print("no position given to %s" % self.name)
             self.position = None
-    
+
+
+def create_circle_of_watchtowers(number, radius, height):
+    if number < 2:
+        print("not enough watchtowers")
+    delta_theta = np.pi/number
+    theta = 0.0
+    list_of_watchtowers = []
+    for i in range(number):
+        t = [radius * np.cos(theta), radius * np.sin(theta), height]
+        x_angle = 5 * np.pi / 6
+        z_angle = theta + np.pi
+        R_z = g.rotation_from_axis_angle(np.array([0, 0, 1]), z_angle)
+        R_x = g.rotation_from_axis_angle(np.array([1, 0, 0]), x_angle)
+        R = np.matmul(R_z, R_x)
+        position = g2o.Isometry3d(R, t)
+        name = "watchtower_%02d" % i
+        watchtower = Watchtower(name, 0.0, position)
+        list_of_watchtowers.append(watchtower)
+        theta += delta_theta
+    return list_of_watchtowers
+
+
+def create_square_of_watchtowers(number, radius, height):
+    if number < 2:
+        print("not enough watchtowers")
+    delta_theta = np.pi/number
+    theta = 0.0
+    list_of_watchtowers = []
+    for i in range(number):
+        t = None
+        if(theta < np.pi/4 or theta > 7*np.pi/4):
+            t = [radius, radius * np.tan(theta), height]
+        elif(3*np.pi/4 < theta < 5*np.pi/4):
+            t = [-radius, radius * np.tan(theta), height]
+        elif(1*np.pi/4 <= theta <= 3*np.pi/4):
+            t = [radius / np.tan(theta), radius, height]
+        else:
+            t = [radius / np.tan(theta), -radius, height]
+        x_angle = 5 * np.pi / 6
+        z_angle = theta + np.pi
+        R_z = g.rotation_from_axis_angle(np.array([0, 0, 1]), z_angle)
+        R_x = g.rotation_from_axis_angle(np.array([1, 0, 0]), x_angle)
+        R = np.matmul(R_z, R_x)
+        position = g2o.Isometry3d(R, t)
+        name = "watchtower_%02d" % i
+        watchtower = Watchtower(name, 0.0, position)
+        list_of_watchtowers.append(watchtower)
+        theta += delta_theta
+
+    return list_of_watchtowers
+
 
 def talker():
     pub = rospy.Publisher('/pose_odometry', TransformStamped, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
     seq = 0
+    list_of_watchtowers = create_circle_of_watchtowers(6,10,5)
+    for watchtower in list_of_watchtowers:
+        print(watchtower.position.t)
+    
+    list_of_watchtowers = create_square_of_watchtowers(6,10,5)
+    for watchtower in list_of_watchtowers:
+        print(watchtower.position.t)
     while not rospy.is_shutdown():
-        # hello_str = "hello world %s" % rospy.get_time()
-        # rospy.loginfo(hello_str)
-        myTransformStamped = TransformStamped()
-        h = std_msgs.msg.Header()
-        h.stamp = rospy.Time.now()
-        s0 = random.randint(0, 5)
-        node_type = "duckie"
-        h.frame_id = "duckie_88"
-        # h.frame_id = "%s_%d" % (node_type, s0)
-
-        myTransformStamped.header = h
-        d = random.randint(0, 5)
-
-        # rotation = [random.random(), random.random(),
-        #             random.random(), random.random()]
-        # rotation = rotation/np.linalg.norm(rotation)
-        # rotation = Quaternion(
-        #     rotation[0], rotation[1], rotation[2], rotation[3])
-        # translation = Vector3(
-        #     d - s0, d - s0 + random.random(), d - s0 + random.random())
-        node_type = random.choice(["duckie", "watchtower"])
-
-        myTransformStamped.transform = Transform(
-            Vector3(0, 0, 0), Quaternion(0, 0, 0, 1))
-        myTransformStamped.child_frame_id = "duckie_88"
-        # myTransformStamped.child_frame_id = "%s_%d" % (node_type, d)
-
-        pub.publish(myTransformStamped)
-
-        rate.sleep()
-        seq += 1
+        pass
 
 
 if __name__ == '__main__':
